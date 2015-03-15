509d6168a4eeac37872931d8932314fa695aaacf
Joshua Kaminetsky <joshua.kaminetsky@twcable.com>
2015-03-15 09:48:47 -0600

    npm, gulp, readme, gitignore, jshint

---
+++ README.md
@@ -0,0 +1,4 @@
+# microservice-generator
+yeoman generator for node microservice
+
+See http://yeoman.io/ for more details about Yeoman.
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+
+require('gulp-help')(gulp);
+
+/**
+ * Runs tests
+ */
+gulp.task('test', 'runs tests', function () {});
